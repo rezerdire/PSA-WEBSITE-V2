63219e5a4dc378325261b0a9d4d99d0c3fa326c5
CHAN <vacaro.267597@gmail.com>
2026-07-20 16:57:41 +0800

    hero section upd: membership form download

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -58,9 +58,10 @@ new class extends Component
             </p>
 
             <div class="au3 flex flex-wrap gap-3 sm:gap-4">
-              <a href="#" class="inline-flex items-center gap-2 px-5 sm:px-6 py-2.5 sm:py-3 bg-blue-600 text-white font-semibold text-sm sm:text-base rounded-xl hover:bg-blue-700 transition-all shadow-md shadow-blue-200 hover:-translate-y-0.5">
-               Membership <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-icon lucide-arrow-right"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
-              </a>
+             <a href="{{ asset('Membership Form.pdf') }}" download class="inline-flex items-center gap-2 px-5 sm:px-6 py-2.5 sm:py-3 bg-red-600 text-white font-semibold text-sm sm:text-base rounded-xl hover:bg-red-700 transition-all shadow-md shadow-blue-200 hover:-translate-y-0.5">
+                Download Membership Form
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-down-icon lucide-file-down"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M12 18v-6"/><path d="m9 15 3 3 3-3"/></svg>
+            </a>
 
               <a href="#recent-events" class="px-5 sm:px-6 py-2.5 sm:py-3 bg-white text-slate-700 font-semibold text-sm sm:text-base rounded-xl border border-slate-200 hover:border-blue-300 hover:text-blue-600 transition-all">
                 Check our events
