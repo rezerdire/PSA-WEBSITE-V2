d33706a43fe60b11c775bdebb9d4dbeedb3d5897
CHAN <vacaro.267597@gmail.com>
2026-07-20 14:24:03 +0800

    sim wars

--- resources/views/pages/Event-Registration/events-registration.blade.php
+++ resources/views/pages/Event-Registration/events-registration.blade.php
@@ -8,13 +8,13 @@ new class extends Component
 };
 ?>
 
-@section('title', 'Event Registration - PSA')
+@section('title', 'Annual Convention Registration 2026')
 @extends('layouts.app')
 @section('content')
 
 <x-about-us-header
     title="Annual Convention 2026"
-    description="Convention & Scientific Meeting" />
+    description="Register for the annual convention 2026!" />
 <x-event-registration.event-registration-layout>
 {{-- Main Content --}}
 <livewire:event-registration.psa-checker />
