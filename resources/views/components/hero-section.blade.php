43925fb18872800e3f4681c125172ab3c55ceef4
CHAN <vacaro.267597@gmail.com>
2026-08-03 10:28:35 +0800

    i forgot </div>

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -123,6 +123,7 @@ new class extends Component
             <img src="{{ asset('images/simwars-competition.png') }}" alt="Anesthesia Sim Wars Trilogy Competition, Episode 1 - Philippines Anesthesia Crisis Competition 2026" class="rounded-2xl shadow-xl shadow-blue-100 max-h-[520px] w-auto object-contain">
         </div>
       </div>
+      </div>
 
       {{-- SLIDE 1: Interesting Case Competition --}}
       <div
