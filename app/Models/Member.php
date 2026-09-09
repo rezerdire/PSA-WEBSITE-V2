c718b7b18ec9ce42d58e0e80f641e2f0713baeac
CHAN <vacaro.267597@gmail.com>
2026-09-09 14:32:38 +0800

    new upd: model member

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -94,8 +94,15 @@ class Member extends Model
         return $this->hasOne(Registration::class, 'psa_id', 'member_id_no');
     }
 
-    public function picture()
+        public function picture()
     {
         return $this->hasOne(MemberPicture::class, 'psa_id', 'member_id_no');
+    }
+
+
+    // qr filament integration
+        public function qr(): HasOne
+    {
+        return $this->hasOne(MemberQr::class, 'member_id_no', 'member_id_no');
     }
 }
